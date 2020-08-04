dcf7d4e5893921fac2bb3d93176788d3269322a5
Alexey Vladimirov <vladimirov.aleksey@gmail.com>
2020-08-04 12:08:58 +0200

    SV19_nnlo_all=0 set added.

--- src/Model/uTMDFF_model.f90
+++ src/Model/uTMDFF_model.f90
@@ -85,18 +85,18 @@ function FNP(x,z,bT,hadron,lambdaNP)
 
     bb=bT**2/x**2
     
-    if(hadron==1) then
+    !if(hadron==1) then
         w1=lambdaNP(1)*x+lambdaNP(2)*(1d0-x)
         w2=lambdaNP(3)
         FNP0=Exp(-bb*w1/sqrt(1d0+w2*bb))*(1+lambdaNP(4)*bb)
         
-    else
-        w1=lambdaNP(5)*x+lambdaNP(6)*(1d0-x)
-        w2=lambdaNP(7)
-        FNP0=Exp(-bb*w1/sqrt(1d0+w2*bb))*(1+lambdaNP(8)*bb)
-    end if
+!     else
+!         w1=lambdaNP(5)*x+lambdaNP(6)*(1d0-x)
+!         w2=lambdaNP(7)
+!         FNP0=Exp(-bb*w1/sqrt(1d0+w2*bb))*(1+lambdaNP(8)*bb)
+!     end if
 
-    FNP=FNP0*(/1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0/)
+    FNP=FNP0*(/1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0,1d0/) 
 
 end function FNP
   
